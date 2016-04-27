42b1012b98a6442e32463d345905a41509b2d822
YorkLu <luyang_1994@foxmail.com>
2016-04-27 18:43:04 +0800

    活动派静态页

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,11 @@
-console.log('This would be the main JS file.');
+/*
+* Author:   YorkLu
+* Date:     2016-04-27
+* */
+
+// 设置标题图长宽比为16:9
+$(document).ready(
+    function () {
+        $(".titleImg").height((0.5 + $(".titleImg").width() / 16 * 9) | 0);
+    }
+);
