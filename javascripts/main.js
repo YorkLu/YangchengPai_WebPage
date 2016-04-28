c382873146178f72be6cffe50cf3259b87d280cd
YorkLu <luyang_1994@foxmail.com>
2016-04-28 17:31:44 +0800

    通过浏览器启动App

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,12 +1,18 @@
 /*
-* Author:   YorkLu
-* Date:     2016-04-27
-* */
+ * Author:   YorkLu
+ * Date:     2016-04-27
+ * */
 
 // 设置标题图长宽比为16:9
 $(document).ready(
     function () {
         $(".titleImg").height((0.5 + $(".titleImg").width() / 16 * 9) | 0);
+
+        /*
+         * TODO:仅测试时使用,发布前请注释掉
+         * ContentGenerator: 内容生成器
+         * */
+        contentGenerator();
     }
 );
 
